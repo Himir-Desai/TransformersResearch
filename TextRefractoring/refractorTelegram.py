f6bd25b4438a6144755198dc361c73afb79eb880
Himir-Desai <himirdesai72@gmail.com>
2023-06-05 12:30:16 +0530

    Added refractoring code for WhatsApp and Telegram

---
+++ TextRefractoring/refractorTelegram.py
@@ -0,0 +1,14 @@
+import json as JSON
+
+f = r"C:\Users\himir\TransformersResearch\TextRefractoring\Telegram\result.json"
+chat = []
+
+with open(f, "r", encoding="utf8") as f:
+    data = JSON.load(f)
+    for message in data["messages"]:
+        if message["type"] == "message":
+            chat.append({message["from"]: message["text"]})
+
+f = r"C:\Users\himir\TransformersResearch\TextRefractoring\chatTelegram.json"
+with open(f, "w", encoding="utf8") as f:
+    JSON.dump(chat, f, ensure_ascii=False, indent=4)
---
+++ TextRefractoring/refractorWhatsApp.py
@@ -0,0 +1,15 @@
+import json as JSON
+
+f = r"C:\Users\himir\TransformersResearch\TextRefractoring\WhatsApp\_chat.txt"
+chat = []
+
+with open(f, "r", encoding="utf8") as f:
+    for text in f:
+        if '\u200e' not in text:
+            P = text[text.find("]")+2:text.find(": ")]
+            Message = text[text.find(": ")+2:]
+            chat.append({P: Message})
+        
+f = r"C:\Users\himir\TransformersResearch\TextRefractoring\chatWhatsApp.json"
+with open(f, "w", encoding="utf8") as f:
+    JSON.dump(chat, f, ensure_ascii=False, indent=4)
