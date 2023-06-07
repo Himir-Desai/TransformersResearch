44024db197c901617c20c53cc40020175f4d60f9
Himir-Desai <himirdesai72@gmail.com>
2023-06-07 11:43:37 +0530

    Added GPT2 in messaging format

--- Project/GPT2Model/gpt2.py
+++ Project/GPT2Model/gpt2.py
@@ -1,10 +1,10 @@
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import torch
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
-model = GPT2LMHeadModel.from_pretrained('gpt2-medium')
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2-large')
+model = GPT2LMHeadModel.from_pretrained('gpt2-large')
 
 def reply(text):
     encoded_input = tokenizer(text, return_tensors='pt')
-    output = model.generate(encoded_input['input_ids'], max_length=50)
+    output = model.generate(encoded_input['input_ids'], max_length=300)
 
     return tokenizer.decode(output[0])
--- Project/app.py
+++ Project/app.py
@@ -1,9 +1,18 @@
 from GPT2Model.gpt2 import reply
 import json as JSON
+import random
 
-f = r"C:\Users\himir\TransformersResearch\Project\TextRefractoring\chatWhatsApp.json"
+f = r"C:\Users\himir\TransformersResearch\Project\TextRefractoring\chatTelegram.json"
 
 with open(f, "r", encoding="utf8") as f:
     chat = JSON.load(f)
-    text = chat[2][list(chat[2].keys())[0]]
+    num = random.randint(0, len(chat) - 6)
+    text = list(chat[num+1].keys())[0] + ": " + chat[num+1][list(chat[num+1].keys())[0]].strip() + "\n"
+    text += list(chat[num+2].keys())[0] + ": " + chat[num+2][list(chat[num+2].keys())[0]].strip() + "\n"
+    text += list(chat[num+3].keys())[0] + ": " + chat[num+3][list(chat[num+3].keys())[0]].strip() + "\n"
+    text += list(chat[num+4].keys())[0] + ": " + chat[num+4][list(chat[num+4].keys())[0]].strip() + "\n"
+    text += list(chat[num+5].keys())[0] + ": " + chat[num+5][list(chat[num+5].keys())[0]].strip() + "\n"
+
+    text += "Himir: "
+
     print(reply(text))
